25efa21db0951a4f95d3b0ca7c62c86eef677ccd
Ghulam Rasool <grsaddiqui75@gmail.com>
2023-10-28 22:58:35 +0500

    set the name of element

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  tilteInputElement: HTMLInputElement;
+  discriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
   constructor() {
     this.templateElement = document.getElementById(
@@ -14,6 +17,16 @@ class ProjectInput {
       true
     );
     this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = "user-input";
+    this.tilteInputElement = this.element.querySelector(
+      "#title"
+    ) as HTMLInputElement;
+    this.discriptionInputElement = this.element.querySelector(
+      "#description"
+    ) as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector(
+      "#people"
+    ) as HTMLInputElement;
     this.attach();
   }
   private attach() {
