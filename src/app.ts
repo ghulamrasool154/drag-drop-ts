d89c10d44eaad9ad4bc6825893affa7faec1233f
Ghulam Rasool <grsaddiqui75@gmail.com>
2023-10-28 23:38:23 +0500

    create submit hnalde an get data

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,24 @@
+/////// AUTO BIND DECORATORE
+
+function autobind(_1: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
+
+////// PROJECT INPUT CLASS
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
   tilteInputElement: HTMLInputElement;
-  discriptionInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
   peopleInputElement: HTMLInputElement;
 
   constructor() {
@@ -22,7 +37,7 @@ class ProjectInput {
     this.tilteInputElement = this.element.querySelector(
       "#title"
     ) as HTMLInputElement;
-    this.discriptionInputElement = this.element.querySelector(
+    this.descriptionInputElement = this.element.querySelector(
       "#description"
     ) as HTMLInputElement;
     this.peopleInputElement = this.element.querySelector(
@@ -32,12 +47,30 @@ class ProjectInput {
     this.configure();
   }
 
+  private gaterUserInput(): [string, string, number] | undefined {
+    const enteredTitle = this.tilteInputElement.value;
+    const enteredDescription = this.descriptionInputElement.value;
+    const enteredPeople = this.peopleInputElement.value;
+
+    if (
+      enteredTitle.trim().length === 0 ||
+      enteredDescription.trim().length === 0 ||
+      enteredPeople.trim().length === 0
+    ) {
+      alert("invalid input, please try again");
+      return;
+    } else {
+      return [enteredTitle, enteredDescription, +enteredPeople];
+    }
+  }
+  @autobind
   private submitHanlder(event: Event) {
     event.preventDefault();
     console.log("event", this.tilteInputElement.value);
+    const userInput = this.gaterUserInput();
   }
   private configure() {
-    this.element.addEventListener("submit", this.submitHanlder.bind(this));
+    this.element.addEventListener("submit", this.submitHanlder);
   }
   private attach() {
     this.hostElement.insertAdjacentElement("afterbegin", this.element);
